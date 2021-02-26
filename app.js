1492ada11ab9028a5a46c23acbbc015951a7508b
ManpreetSingh <manpreetsinghpuri01@gmail.com>
2021-02-27 01:36:15 +0530

    Created a basic structure of TODO

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.set('view engine', 'ejs');
+app.listen(3000);
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/add-items", (req, res) => {
+    res.render('add-items');
+})
+
+app.use((req, res) => {
+    res.render('error');
+})
